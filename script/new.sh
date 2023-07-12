ff5f1d569b713a5e4e0c15864d91f3c72c933e82
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-12 16:01:50 +0800

    patch ready

--- script/new.sh
+++ script/new.sh
@@ -2,7 +2,7 @@
 sed -i "" "s/nodept/$1/g" package.json
 sed -i "" "s/nodept/$1/g" package-lock.json
 sed -i "" "s/nodept/$1/g" README.md
-rm -rf .git
-git init
-git add .
-git commit -m 'init'
+# rm -rf .git
+# git init
+# git add .
+# git commit -m 'init'

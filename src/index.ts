3b937ec7da411bc5e21a090eee3488d836062b05
jimao <shihao.gu@perfma.com>
2024-07-29 11:46:09 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import http from 'http';
 
 export
 function hello() {
+  traverse(http, (key, value) => {
+    console.log(key, value);
+    return value;
+  })
   console.log('hello world!');
 }
 
@@ -21,4 +26,20 @@ type Proto =
 export
 function proto(data: any) {
   return Object.prototype.toString.call(data) as Proto;
+}
+
+export
+function traverse(
+  data: any,
+  mapping: (key: string, value: any) => any,
+  parentKey = '$',
+) {
+  const prototype = proto(data);
+  if (prototype === '[object Object]' || prototype === '[object Array]') {
+    Object.entries(data).forEach(([key, value]) => {
+      const newKey = `${parentKey}['${key}']`;
+      data[key] = traverse(value, mapping, newKey);
+    });
+  }
+  return mapping(parentKey, data);
 }

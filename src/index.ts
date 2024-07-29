aa01a0d2ee78ac67b56d8416477f0d0e869a5db1
jimao <shihao.gu@perfma.com>
2024-07-29 11:07:40 +0800

    proto

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,22 @@ export
 function hello() {
   console.log('hello world!');
 }
+
+export
+type Proto =
+  '[object Object]' |
+  '[object Array]' |
+  '[object String]' |
+  '[object Number]' |
+  '[object Boolean]' |
+  '[object Function]' |
+  '[object Date]' |
+  '[object RegExp]' |
+  '[object Error]' |
+  '[object Null]' |
+  '[object Undefined]';
+
+export
+function proto(data: any) {
+  return Object.prototype.toString.call(data) as Proto;
+}
